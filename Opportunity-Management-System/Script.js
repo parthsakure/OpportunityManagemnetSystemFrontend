abaf36ec9f60e0c6a00be2e2dc8276b7184297e7
Parth <56023417+parthsakure@users.noreply.github.com>
2023-08-29 11:48:08 +0530

    code cleaning

--- Opportunity-Management-System/Script.js
+++ Opportunity-Management-System/Script.js
@@ -5,25 +5,4 @@ const data = [{"opportunityId":2,"title":"Parth's","description":"description","
 {"opportunityId":2,"title":"Parth's","description":"description","primaryNeed":"to test my apis","expectedMonthlyRevenue":1973321.00,"expectedLaunchDate":"2024-09-06T00:30:00Z","closedLostReason":"not working", dealStage: {value:"approved"}},]
 
 
-createTable(document.querySelector("#app"),[
-    {
-        name: "title"
-    },
-    {
-        name: "description"
-    },
-    {
-        name: "primaryNeed"
-    },
-    {
-        name: "expectedMonthlyRevenue"
-    },
-    {
-        name: "expectedLaunchDate"
-    },
-    {
-        name: "deal Stage",
-        entityName:"dealStage"
-    }],
-    data,
-    true);
+createTable(document.querySelector("#app"),data,true);
--- Opportunity-Management-System/table.js
+++ Opportunity-Management-System/table.js
@@ -1,68 +1,60 @@
+function createCell(value, classes){
+    const cellElement = document.createElement("td");
+    if(classes){
+        cellElement.classList.add(...classes);
+    }
+    cellElement.innerText = value;
+    return cellElement;
+}
 
 
-function createTable(rootElement, headers, data, srno=false){
-    table = document.createElement("table");
-    table.classList.add("table","table-striped");
-    table.classList.add("border");
-    table.style.width = "80%";
-    table.style.margin = "auto";
+function createColumn(value, classes){
+    const columnElement = document.createElement("th");
+    if(classes) columnElement.classList.add(...classes);
+    columnElement.innerText = value;
+    return columnElement;
+}
 
-    const headerElement = document.createElement("thead");
-    headerElement.classList.add("border");
+function createRow(row, headers, classes){
+    //create new row Element
     const rowElement = document.createElement("tr");
 
-    if(srno){
-        const srNoColumn = document.createElement("th");
-        // srNoColumn.classList.add("text-left");
-        srNoColumn.innerText = "Sr.No.";
-        rowElement.appendChild(srNoColumn);
-    }
+    //Add Classes if available
+    if(classes) rowElement.classList.add(...classes);
+
+    //Add values to the row
+    headers.forEach(column =>{
+        const cell = row[column.value ? column.value : column];
+        rowElement.appendChild(createCell(cell.value ? cell.value : cell, cell.classes))        
+    })
+    return rowElement;
+}
+
+function createTable(rootElement, data, append=false){
+    //Create Table element
+    const headers= Object.keys(data[0]);
+    table = document.createElement("table");
+    table.classList.add("table","table-striped");
+
+    //create table header
+    const headerElement = document.createElement("thead");
+    const rowElement = document.createElement("tr");
+    //Add columns to table
     headers.forEach(column => {
-        const columnElement = document.createElement("th");
-        columnElement.classList.add("text-left");
-        columnElement.innerText = column.name;
-        rowElement.appendChild(columnElement);
+        rowElement.appendChild(createColumn(column, ['text-left']));
     });
-
     headerElement.appendChild(rowElement);
-
     table.appendChild(headerElement);
 
+    //create body element
     const bodyElement = document.createElement("tbody")
-    let i= 1;
     data.forEach(row=>{
-        const rowElement = document.createElement("tr");
-        rowElement.style.height="2em";
-        if(i%2){
-            rowElement.classList.add("bg-gray-200");
-        }
-        if(srno){
-            const srNoColumn = document.createElement("th");
-            srNoColumn.innerText = i;
-            rowElement.appendChild(srNoColumn);
-        }
-        headers.forEach(column=>{
-            const cellElement = document.createElement("td");
-            const cell = row[column.entityName ? column.entityName : column.name];
-            cellElement.innerHTML = cell.value ? cell.value : cell;
-            if(column.classes){
-                column.classes.forEach(cls=>{
-                    cellElement.classList.add(cls);
-                });
-            }
-            if(cell.classes){
-                cell.classes.forEach(cls=>{
-                    cellElement.classList.add(cls);
-                });
-            }
-            rowElement.appendChild(cellElement);
-        });
-        bodyElement.appendChild(rowElement);
-        i++;
+        bodyElement.appendChild(createRow(row.value ? row.value : row, headers, row.classes));
     });
     table.appendChild(bodyElement);
 
-    rootElement.innerHTML = "";
+    //erase everything from root element
+    if(!append) rootElement.innerHTML = "";
 
     rootElement.appendChild(table);
 }
