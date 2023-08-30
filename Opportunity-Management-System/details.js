e0bc5bcebb9ede762232c81b77fa51db2a171522
Parth <56023417+parthsakure@users.noreply.github.com>
2023-08-30 10:26:26 +0530

    preprocessing opportunity data

--- Opportunity-Management-System/details.js
+++ Opportunity-Management-System/details.js
@@ -1,6 +1,6 @@
 function createField(key, value, classes){
-    const root = document.createElement("p");
-    root.classList.add("row")
+    const root = document.createElement("span");
+    root.classList.add("row","row-cols-2")
     const keyElement = document.createElement("strong");
     keyElement.innerHTML = key + ": ";
     root.appendChild(keyElement);
@@ -9,7 +9,7 @@ function createField(key, value, classes){
     valueElement.innerHTML = value;
     root.appendChild(valueElement);
     keyElement.classList.add("col","text-end");
-    valueElement.classList.add("col");
+    valueElement.classList.add("col-auto");
 
     if(classes["key"]){
         keyElement.classList.add(...classes.key);
@@ -30,7 +30,6 @@ function showDetailedView(rootElement, title, data, append=false){
     if(!append) rootElement.innerHTML = "";
     rootElement.appendChild(createTitle(title));
     Object.keys(data).forEach(key=>{
-        console.log(data[key]);
         rootElement.append(createField(key, data[key].value ? data[key].value : data[key], data[key].classes ? data[key].classes : []));
     });
     return rootElement;
---
+++ Opportunity-Management-System/prepareOpportunity.js
@@ -0,0 +1,62 @@
+function getDealStageClasses(dealstage){
+    classes = [];
+    classes.push("border");
+    console.log(dealstage.toLowerCase())
+    switch (dealstage.toLowerCase()) {
+        case "approved":
+            classes.push("border-info-subtle")
+            break;
+        
+        case "closed/lost":
+            classes.push("bg-danger-subtle")
+            break;
+
+        case "launched":
+            classes.push("bg-success-subtle")
+            break;
+        default:
+            classes.push("border-warning")
+            classes.push("bg-warning-subtle")
+            break;
+    }
+    return {value: dealstage,classes:classes};
+}
+
+
+function prepareOpportunityForTableData(data,isAdmin=false){
+    let finalData = [];
+    data.forEach(row => {
+        let opportunity = {};
+        opportunity["title"] = row["title"];
+
+        if(isAdmin) opportunity["Deal Owner"] = "@" + row["dealOwner"]["username"];
+
+        opportunity["description"] = row["description"];
+        opportunity["deal Stage"]= row["dealStage"]["dealStage"];
+
+        let classes = [];
+        if(!row["active"]){
+            classes.push("disabled");
+        }
+        
+        finalData.push(opportunity);
+    });
+    return finalData;
+}
+
+
+function prepareOpportunityDetails(data,isAdmin=false){
+    let opportunity = {};
+    opportunity["Title"] = data["title"];
+    if(!isAdmin) opportunity["Deal Owner"] = "@" + data["dealOwner"]["username"];
+    opportunity["Description"] = data["description"];
+    opportunity["Primary Need"] = data["primaryNeed"];
+    opportunity["Expected Monthly Revenue"] = data["expectedMonthlyRevenue"];
+    opportunity["Expected Launch Date"] = data["expectedLaunchDate"];
+    opportunity["Active"] = {value: data["active"],classes:{value:[data["active"] ? "bg-success-subtle" : "bg-light-subtle"]}};
+    opportunity["Delivery Model"] = data["deliveryModel"]["deliveryModel"];
+    opportunity["Deal Stage"] = getDealStage(data["dealStage"]["dealStage"]);
+
+    return opportunity;
+}
+
