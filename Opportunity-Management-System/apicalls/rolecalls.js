55f958485b3d227b229dfbaedbda8c7dad698ce8
Arya <aryashanbhag3@gmail.com>
2023-08-31 13:04:33 +0530

    fix roles api call

--- Opportunity-Management-System/apicalls/rolecalls.js
+++ Opportunity-Management-System/apicalls/rolecalls.js
@@ -1,6 +1,6 @@
 async function getRoles()
 {
-    var url = BASE_URL+"opportunity/";
+    var url = BASE_URL+"roles/";
     const response = await fetch(url);
     var data = await response.json();
     console.log(data);
