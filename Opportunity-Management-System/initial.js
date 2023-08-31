e39e20c2fd30745bc3550f788cc632d55e54e0dc
Parth <56023417+parthsakure@users.noreply.github.com>
2023-08-31 12:52:46 +0530

    Authentication Added

--- Opportunity-Management-System/initial.js
+++ Opportunity-Management-System/initial.js
@@ -1,6 +1,26 @@
+const BASE_URL= "https://opportunity-management.onrender.com";
+const TOKEN_NAME = "jwt_token"
+
+
 function createTitle(title){
     const h1 = document.createElement("h1");
     h1.textContent = title;
     h1.classList.add("fs-1","mb-5","mt-3","ms-5")
     return h1;
+}
+
+function HandleToken(response){
+    localStorage.setItem(TOKEN_NAME, response.credential);
+}
+
+
+function getHeaders(){
+    const token = localStorage.getItem(TOKEN_NAME);
+
+    if(token==null){
+        return {};
+    }
+
+    return {"Authorization": "Bearer "+token};
+
 }
