803387ed86e7b3ee714ae8e022388e202cb64464
Parth <56023417+parthsakure@users.noreply.github.com>
2023-08-31 13:10:56 +0530

    bug fixed

--- Opportunity-Management-System/initial.js
+++ Opportunity-Management-System/initial.js
@@ -9,18 +9,28 @@ function createTitle(title){
     return h1;
 }
 
-function HandleToken(response){
-    localStorage.setItem(TOKEN_NAME, response.credential);
-}
 
-
-function getHeaders(){
+function getHeader(){
     const token = localStorage.getItem(TOKEN_NAME);
-
     if(token==null){
         return {};
     }
+    return {Authorization: "Bearer "+token};
+}
 
-    return {"Authorization": "Bearer "+token};
 
+function parseJwt (token) {
+    var base64Url = token.split('.')[1];
+    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+    
+    return JSON.parse(jsonPayload);
+}
+
+
+function saveToken(data){
+    // console.log(parseJwt(data))
+    localStorage.setItem(TOKEN_NAME, data);
 }
