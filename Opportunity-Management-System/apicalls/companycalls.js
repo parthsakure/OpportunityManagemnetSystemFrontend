d0ea5745f7123ff777e1397764627baf68b1aa3d
Parth <56023417+parthsakure@users.noreply.github.com>
2023-08-31 18:01:21 +0530

    navbar created

--- Opportunity-Management-System/apicalls/companycalls.js
+++ Opportunity-Management-System/apicalls/companycalls.js
@@ -82,3 +82,5 @@ async function postCompany()
         console.log("Error:", error);
     }
 }
+
+
---
+++ Opportunity-Management-System/navbar.js
@@ -0,0 +1,22 @@
+function createNavItem(data){
+    const item = document.createElement("li");
+    item.classList.add("nav-item",'mx-3','fs-5');
+    const aTag = document.createElement("a");
+    aTag.classList.add("nav-link")
+    aTag.setAttribute("href",'#');
+    aTag.setAttribute('onClick',data['callback']+'()')
+    aTag.innerText = data['value'];
+    item.appendChild(aTag);
+   return item;
+}
+
+
+function createNavBar(data) {
+    const list = document.createElement("ul");
+    list.classList.add("navbar-nav",'me-auto','mb-2');
+    data.forEach(link => {
+        list.appendChild(createNavItem(link));
+    });
+    return list;
+}
+
