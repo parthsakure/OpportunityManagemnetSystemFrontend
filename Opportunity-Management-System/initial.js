c4bf32ab33b9243b3153e7f4def380f14c7228d5
amanlad97 <76096700+amanlad97@users.noreply.github.com>
2023-08-31 20:23:24 +0530

    minor changes

--- Opportunity-Management-System/initial.js
+++ Opportunity-Management-System/initial.js
@@ -32,8 +32,6 @@ function createTitle(title){
     h1.classList.add("fs-1","mb-5","mt-3","ms-5")
     return h1;
 }
-
-
 function getHeader(){
     const token = localStorage.getItem(TOKEN_NAME);
     if(token==null){
@@ -43,19 +41,6 @@ function getHeader(){
     'Content-type': 'application/json'
 };
 }
-
-
-function parseJwt (token) {
-    var base64Url = token.split('.')[1];
-    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-    var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
-        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-    }).join(''));
-    
-    return JSON.parse(jsonPayload);
-}
-
-
 function saveToken(data){
     localStorage.setItem(TOKEN_NAME, data);
     location.reload();
