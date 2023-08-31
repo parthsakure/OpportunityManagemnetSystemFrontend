06b438f64588144be9c81e1a1314e9b35049f111
Arya <aryashanbhag3@gmail.com>
2023-08-31 18:32:21 +0530

    ;

--- Opportunity-Management-System/apicalls/locationcalls.js
+++ Opportunity-Management-System/apicalls/locationcalls.js
@@ -62,4 +62,4 @@ async function postLocation(data)
     }
 }
 
-export default {getLocations, getLocationById, updateLocation, postLocation}
+export default {getLocations, getLocationById, updateLocation, postLocation};
